0eab8e21c8124915178ea183590d39514dbf42fc
ItsAzM8 <owens.darcy67@gmail.com>
2020-08-03 22:21:52 +0930

    changed rate

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from vosk import Model, KaldiRecognizer
 import os
 import json
+import pyaudio
+
+RATE = 44100
 
 if not os.path.exists("model"):
     print("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack "
-          "as 'model' in the current folder.")
+          "as 'model' in the current folder. For this project, vosk-model-en-us-daanzu is recommended")
     exit(1)
 
-import pyaudio
 
 model = Model("model")
-rec = KaldiRecognizer(model, 16000)
+rec = KaldiRecognizer(model, RATE)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
 while True:
@@ -21,4 +23,3 @@ while True:
     if rec.AcceptWaveform(data):
         result = json.loads(rec.Result())
         print(result["text"])
-
